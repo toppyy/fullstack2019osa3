9fb1e51e09c2ea1c026450610ab4a8f0a127671f
topiasp <topias.pyykkonen@gmail.com>
2019-06-15 16:21:51 +0300

    eslint

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
 
 const url = process.env.MONGODB_URI
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -15,17 +15,17 @@ mongoose.connect(url, { useNewUrlParser: true })
   })
 
 const personSchema = new mongoose.Schema({
-    name: {
-     type: String,
-     required: true,
-     minlength: 3,
-     unique: true 
-    },
-    number: {
-      type: String,
-      required: true,
-      minlength: 8
-    }
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    unique: true
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8
+  }
 })
 
 
